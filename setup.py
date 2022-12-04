3c095dbf86ff725e196c66afe8c2d4d2d66cab8a
T-Lind <tiernanxklindauer@gmail.com>
2022-12-04 16:48:49 -0600

    setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='',
+    version='0.1',
+    license='MIT',
+    author="Tiernan Lindauer",
+    author_email='tiernanxkl@gmail.com',
+    packages=find_packages('MotorLib'),
+    package_dir={'': 'MotorLib'},
+    url='https://github.com/T-Lind/MotorLib',
+    keywords='Robotics',
+    install_requires=[
+          'RPi.GPIO',
+      ],
+
+)
